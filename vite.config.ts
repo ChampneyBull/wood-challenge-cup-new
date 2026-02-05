8bf11fb91ef89f94488b6a2270d7d36074d51136
ChampneyBull <79786636+ChampneyBull@users.noreply.github.com>
2026-02-05 10:30:10 +0000

    correct data skipton Chester

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-    build: {
-      outDir: 'dist',
-    }
-  }
-
-  if (command !== 'serve') {
-    config.base = '/wood-challenge-cup-new/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: '/wood-challenge-cup-new/',
+  build: {
+    outDir: 'docs',
+  },
 })
